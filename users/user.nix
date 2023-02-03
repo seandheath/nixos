8d8ab7b1a923a173e90d8d3c765c097e07d14436
Sean Heath <se@nheath.com>
2023-02-03 07:35:28 -0500

    updated user

--- users/user.nix
+++ users/user.nix
@@ -2,7 +2,7 @@
 {
   users.users.user = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "libvirtd" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLhPOBx9dR2X3oYz5RS2eAGZA7YSeHPcnrQauHSmuk1"
     ];
