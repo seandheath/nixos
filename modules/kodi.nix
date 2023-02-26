aceef8dab4e023581a956db89b67447b04d8f46f
Sean Heath <heathsd@pm.me>
2023-02-26 17:40:26 -0500

    added sound to kodi

--- modules/kodi.nix
+++ modules/kodi.nix
@@ -2,8 +2,16 @@
   services.xserver.enable = true;
   services.xserver.desktopManager.kodi.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.user = "kodi";
+  services.xserver.displayManager.autoLogin.user = "kodi";
   services.xserver.displayManager.lightdm.autoLogin.timeout = 3;
   users.extraUsers.kodi.isNormalUser = true;
-  users.extraUsers.kodi.extragroups = [ "usenet" ];
+  users.extraUsers.kodi.extraGroups = [ "usenet" ];
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
