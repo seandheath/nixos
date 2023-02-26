b48963ca95cfce10e5477c0cead8431e14f03477
Sean Heath <heathsd@pm.me>
2023-02-25 22:19:06 -0500

    added systemd config for jellyfin

--- modules/usenet.nix
+++ modules/usenet.nix
@@ -30,4 +30,9 @@ in {
     user = "usenet";
     group = "usenet";
   };
+  systemd.services."jellyfin".serviceConfig = {
+    DeviceAllow = pkgs.lib.mkForce [ "char-drm rw" "char-nvidia-frontend rw" "cahr-nvidia-uvm rw" ];
+    PrivateDevices = pkgs.lib.mkForce true;
+    RestrictAddressFamilies = pkgs.lib.mkForce [ "AF_UNIX" "AF_NETLINK" "AF_INET6" ];
+  };
 }
