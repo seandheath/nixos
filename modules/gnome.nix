b753e0087a9335b9b0acaa37f5be31289e21c3a6
Sean Heath <heathsd@pm.me>
2023-08-11 06:21:17 -0400

    added p7zip to gnome module

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:{
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [
+    p7zip
     obsidian
     joplin-desktop
     pavucontrol
