2d161fb09faef866a5528527ed812f04598eeddf
Sean <se@nheath.com>
2024-01-02 11:07:00 -0500

    added semicolon

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -124,7 +124,7 @@
       options = [
         "noatime"
         "nodiratime"
-      ]
+      ];
     };
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
