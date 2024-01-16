009dbe5215d94ca68c75934ceb2ac4f216edb7c4
Sean Heath <se@nheath.com>
2024-01-16 16:02:57 -0500

    semi

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   imports = [
     <sops-nix/modules/sops>
-  ]
+  ];
   sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.age.keyFile = "/root/sops-key.txt";
   sops.age.generateKey = false;
