ce3be759af33706cdcee4e1168782f3f4f532a3a
Sean Heath <seanheath87@gmail.com>
2025-09-24 14:08:49 -0400

    fixed password hash

--- users/sheath.nix
+++ users/sheath.nix
@@ -1,9 +1,16 @@
+{ config, pkgs, ... }:
+
 {
-  isNormalUser = true;
-  description = "sheath";
-  extraGroups = [ "wheel" "networkmanager" "video" "libvirtd" "podman" ];
-  openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLhPOBx9dR2X3oYz5RS2eAGZA7YSeHPcnrQauHSmuk1"
-  ];
-  group = "sheath";
+  users.users.sheath = {
+    isNormalUser = true;
+    description = "sheath";
+    extraGroups = [ "wheel" "networkmanager" "video" "libvirtd" "podman" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGLhPOBx9dR2X3oYz5RS2eAGZA7YSeHPcnrQauHSmuk1"
+    ];
+    group = "sheath";
+    hashedPasswordFile = config.sops.secrets.sheath_password_hash.path;
+  };
+
+  sops.secrets.sheath_password_hash = {};
 }
