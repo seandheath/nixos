385499c0b8dc5acd87d69a8d669f51006d1f3be6
Sean Heath <seanheath87@gmail.com>
2026-04-02 09:01:41 -0400

    fix(kitty): correct font name to B612 Mono

--- home/kitty.nix
+++ home/kitty.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     font.package = pkgs.b612;
-    font.name = "Inconsolata";
+    font.name = "B612 Mono";
     font.size = 11;
     settings = {
       shell = "bash --login";
