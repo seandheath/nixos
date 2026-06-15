d9db5e0061c124a4853b33a92a7790b5c92b6a21
Sean Heath <seanheath87@gmail.com>
2026-06-15 16:30:07 -0400

    chore(hydrogen): fix deprecated option names for 25.11
    Rename to current options (no functional change; identical system derivation):
    - services.xserver.displayManager.{gdm,autoLogin} -> services.displayManager.*
    - services.xserver.desktopManager.gnome -> services.desktopManager.gnome
    - services.xserver.{layout,xkbVariant} -> services.xserver.xkb.{layout,variant}
    - hardware.pulseaudio -> services.pulseaudio

--- hosts/hydrogen.nix
+++ hosts/hydrogen.nix
@@ -88,17 +88,17 @@
   virtualisation.libvirtd.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -108,8 +108,8 @@
   };
 
   # Enable automatic login for the primary user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "sheath";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "sheath";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
