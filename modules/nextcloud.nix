fb6b9493016c7bc38278f69684b4b900f64bc73e
Sean Heath <seanheath87@gmail.com>
2026-08-19 17:05:19 -0400

    feat(nextcloud): 32 -> 33 on hydrogen

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -8,7 +8,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
     hostName = "nc.luckyobserver.com";
     https = true;
     # Data lives on root (/var/lib/nextcloud); backed up via Borg (modules/backup.nix).
