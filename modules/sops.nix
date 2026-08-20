d9be5f1c2eb86ed18cd6378ac630f17c7197d6c5
Sean Heath <seanheath87@gmail.com>
2026-08-19 21:29:09 -0400

    fix(sops): disable missing host-key fallback

--- modules/sops.nix
+++ modules/sops.nix
@@ -5,4 +5,8 @@
 {
   sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.age.keyFile = "${config.users.users.sheath.home}/.config/sops/age/keys.txt";
+  # The installer provisions the age key before activation.  Do not make fresh installs
+  # probe host SSH keys that do not exist until OpenSSH is activated.
+  sops.age.sshKeyPaths = [ ];
+  sops.gnupg.sshKeyPaths = [ ];
 }
